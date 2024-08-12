93b91d1932fc9fdc36e72fb928dd5e64a6ab7f33
agripe049 <agripeheber@gmail.com>
2024-08-12 19:25:54 -0300

    servidor dinamico

--- src/components/pages/Projetos.js
+++ src/components/pages/Projetos.js
@@ -25,7 +25,7 @@ function Projetos () {
     useEffect(() => {
         setTimeout(
             () => {
-                fetch('http://localhost:5000/projects', {
+                fetch(`${process.env.REACT_SERVER_API}projects`, {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',
@@ -41,7 +41,7 @@ function Projetos () {
     }, [])
 
     function removeProjeto(id) {
-        fetch(`http://localhost:5000/projects/${id}`, {
+        fetch(`${process.env.REACT_SERVER_API}projects/${id}`, {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json'
